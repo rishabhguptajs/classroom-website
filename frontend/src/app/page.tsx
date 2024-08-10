a2a611dc39ea4523825627f5716392961cc9852f
rishabhguptajs <rishabhgupta4523@gmail.com>
2024-08-10 16:52:56 +0530

    feat: Update Home page UI to display user email and add link to Dashboard

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -19,11 +19,16 @@ export default function Home() {
 
       <div className="mt-8 flex flex-col items-center space-y-6">
         {user ? (
-          <div className="text-center">
-            <h2 className="text-3xl font-semibold text-white">
+          <div className="text-center flex flex-col items-center">
+            <h2 className="text-2xl mb-4 font-semibold text-white">
               Hello, {user.email}!
             </h2>
-            <button className="mt-6 px-6 py-3 bg-red-500 hover:bg-red-600 text-white font-bold rounded-lg shadow-lg transform transition duration-200 hover:scale-105 focus:outline-none"
+            <Link href={`/${user.role.toLowerCase()}/dashboard`}>
+              <span className="m-4 px-6 py-3 bg-blue-500 hover:bg-blue-600 text-white font-bold rounded-lg shadow-lg transform transition duration-200 hover:scale-105 focus:outline-none">
+                Go to Dashboard
+              </span>
+            </Link>
+            <button className="mt-6 px-6 py-3 w-fit bg-red-500 hover:bg-red-600 text-white font-bold rounded-lg shadow-lg transform transition duration-200 hover:scale-105 focus:outline-none"
               onClick={logout}
             >
               Logout
